2b21d90136fb6b0171ff8e677940df3b99554204
Justin Pau <jupau@berkeley.com>
2023-07-01 11:38:30 -0700

    widgets:

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className="wrapper">
         <div className="search">
           <input type="text" placeholder = "Search..." />
-          <SearchOutlinedIcon/>
+          <SearchOutlinedIcon className="icon"/>
         </div>
         <div className="items">
           <div className="item">
